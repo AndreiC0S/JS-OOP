46e52a644a65e67d18e0a60a1a111611af9016af
AndreiCos <frontendjavascript@C3-02.local>
2023-03-28 20:47:39 +0300

    curs2

--- curs1/js/carte.js
+++ curs1/js/carte.js
@@ -1,22 +1,44 @@
 class Carte{
-    constructor(titlu,autor,editura,an_aparitie,pret,nr_pagini){
+    static reducere = 10;
+    myPret;
+    constructor(titlu,autor,editura,an_aparitie,nr_pagini){
         this.titlu = titlu;
         this.autor = autor;
         this.editura = editura;
         this.an_aparitie = an_aparitie;
-        this.pret = pret;
+        
         this.nr_pagini = nr_pagini;
     }
+    get pret(){
+        console.log('sunt in getter')
+        return this.myPret;
+    }
+    set pret(pretNou){
+        console.log('sunt in setter')
+        try{
+            if(pretNou <= 0){
+                throw 'pretul nu poate fi actualizat'
+            } else{
+                this.myPret = pretNou;
+            }
+        }catch{
+            console.error('f');
+        }
+        
+    }
+    // pretNou(){
+    //     this.pret = this.pret -(Carte.reducere * this.pret) / 100
+    // }
     obtine_titlu(){
         console.log(`titlu: ${this.titlu}`)
     }
     obtine_autor(){
         console.log(`autor: ${this.autor}`)
     }
-    modifica_pret(pretNou){
-        this.pret = pretNou;
-        console.log(`noul pret: ${this.pret}`)
-    }
+    // modifica_pret(pretNou){
+    //     this.pret = pretNou;
+    //     console.log(`noul pret: ${this.pret}`)
+    // }
     afiseaza_info(){
         console.log(`anul aparitiei: ${this.an_aparitie} si are ${this.nr_pagini} pagini`)
     }
@@ -28,6 +50,8 @@ class Carte{
         }
     }
 }
+
+
 
 
 
